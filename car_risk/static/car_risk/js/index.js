afdff72589d104f4efaf21305f21d8d13f940dda
Devsabirul <mdsabirulislam8089@gmail.com>
2022-06-30 22:25:57 +0600

    change print function

--- car_risk/static/car_risk/js/index.js
+++ car_risk/static/car_risk/js/index.js
@@ -1,6 +1,15 @@
+
 const predict_car_risk = document.getElementById('predict_carrisk');
 
 
 predict_car_risk.onclick = (event) => {
     document.querySelector(".car_result").style.display = "block";
+}
+
+// for print function 
+
+const clicked = document.getElementById("print");
+
+clicked.onclick = (e) => {
+    window.print();
 }
